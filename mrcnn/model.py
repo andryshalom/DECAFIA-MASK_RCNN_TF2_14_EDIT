cf8b62d5e4ebcf643c477c2f38519368a60e6cb2
andryshalom <155601453+andryshalom@users.noreply.github.com>
2024-01-13 18:17:18 -0500

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2345,7 +2345,7 @@ class MaskRCNN(object):
         # Callbacks
         callbacks = [
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
-                                        histogram_freq=1, write_graph=True, write_images=False),
+                                        histogram_freq=0, write_graph=True, write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
                                             verbose=0, save_weights_only=True),
         ]
